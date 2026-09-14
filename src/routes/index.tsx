067dc9ac2ce512d81ebd743e2ca80bca26a88e29
orbioncontato-collab <orbioncontato@gmail.com>
2026-09-14 13:58:40 -0300

    Attach enhanced wheel experience to gamification MVP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { GamificationApp } from "../gamification-app";
+import { WheelExperience } from "../wheel-experience";
 
 export const Route = createFileRoute("/")({
-  component: GamificationApp,
+  component: Index,
 });
+
+function Index() {
+  return (
+    <>
+      <GamificationApp />
+      <WheelExperience />
+    </>
+  );
+}
